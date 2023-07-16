9323823d1d72df88e0a990f16a7feedaa6e46d6a
Mike Dawson <mike@ustadmobile.com>
2023-07-16 14:11:20 +0400

    Improve forwarding: update virtualnodereturnpathsocketfactory so that the socket is bound to the network interface where the subnet matches the destination.

--- lib-httpoverbluetooth/src/main/java/com/ustadmobile/meshrabiya/vnet/VirtualNodeReturnPathSocketFactory.kt
+++ lib-httpoverbluetooth/src/main/java/com/ustadmobile/meshrabiya/vnet/VirtualNodeReturnPathSocketFactory.kt
@@ -2,16 +2,34 @@ package com.ustadmobile.meshrabiya.vnet
 
 import com.ustadmobile.meshrabiya.ext.prefixMatches
 import com.ustadmobile.meshrabiya.portforward.ReturnPathSocketFactory
+import java.lang.IllegalArgumentException
 import java.net.DatagramSocket
 import java.net.InetAddress
+import java.net.NetworkInterface
+import java.util.Enumeration
 
 /**
  * Implementation of return path socket factory that can create an IDatagramSocket for the real
- * local loopback or the virtual network.
+ * network or a virtual datagram socket.
+ *
+ * If a destination is on the real network, then the created socket will be bound to the network
+ * interface where the netmask matches the given destination address.
  */
 class VirtualNodeReturnPathSocketFactory(
     private val node: VirtualNode,
 ): ReturnPathSocketFactory {
+
+    private inline fun <T, R> Enumeration<T>.firstNotNullOfOrNull(
+        transform: (T) -> R?
+    ): R? {
+        while(hasMoreElements()) {
+            val transformed = transform(nextElement())
+            if(transformed != null)
+                return transformed
+        }
+
+        return null
+    }
 
     override fun createSocket(destAddress: InetAddress, port: Int): IDatagramSocket {
         return if(
@@ -19,7 +37,21 @@ class VirtualNodeReturnPathSocketFactory(
         ) {
             node.openSocket(port)
         }else{
-            DatagramSocket().asIDatagramSocket()
+            val bindAddress = NetworkInterface.getNetworkInterfaces().firstNotNullOfOrNull { netInterface ->
+                netInterface.interfaceAddresses.firstNotNullOfOrNull { interfaceAddress ->
+                    if(interfaceAddress.address.prefixMatches(
+                            interfaceAddress.networkPrefixLength.toInt(), destAddress)
+                    ) {
+                        interfaceAddress.address
+                    }else {
+                        null
+                    }
+                }
+            }
+
+            return bindAddress?.let { DatagramSocket(0, it).asIDatagramSocket() }
+                ?: throw IllegalArgumentException("Could not find network interface with subnet " +
+                        "mask for dest address $destAddress")
         }
     }
 
